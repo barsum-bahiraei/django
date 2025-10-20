245f9fbf842936627aa69d74fecf263e55ad20de
barsum <barsum.bahiraei@gmail.com>
2025-10-20 14:29:04 +0330

    .

--- store/apps/products/models.py
+++ store/apps/products/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+    class Meta:
+        db_table = 'products'
