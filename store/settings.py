7fc2ea66eadfb46a8b94db94de0552b5c7333c80
barsum <barsum.bahiraei@gmail.com>
2025-10-20 13:58:05 +0330

    .

--- store/settings.py
+++ store/settings.py
@@ -73,9 +73,17 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'StoreDb',
+        'USER': 'barsum',
+        'PASSWORD': 'DataB@se@1234',
+        'HOST': '127.0.0.1',  # یا 'postgres' اگر داخل Docker هستید
+        'PORT': '5432',
     }
 }
 
